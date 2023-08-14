3feab2510ebf9ff2c91071bdcd037394e7fe3b22
Andre-42 <>
2023-08-14 12:00:17 +0000

    get and check sales data function complete

--- run.py
+++ run.py
@@ -15,15 +15,20 @@ def get_sales_data():
     """
     Get sales figures from the user
     """
-    print("Please enter sales data from last market!")
-    print("Data should be six numbers,")
-    print("Example: 10,20,30,40,60")
+    while True:
+        print("Please enter sales data from last market!")
+        print("Data should be six numbers,")
+        print("Example: 10,20,30,40,60")
 
-    data_str = input("Enter your data here: ")
-    print(f" The data provided is {data_str}")
+        data_str = input("Enter your data here: ")
+        print(f" The data provided is {data_str}")
 
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+        sales_data = data_str.split(",")
+        
+        if validate_data(sales_data):
+            print("Data is valid")
+            break
+    return sales_data
 
 def validate_data(values):
     """ 
@@ -39,5 +44,6 @@ def validate_data(values):
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
+        return False
 
-get_sales_data()
+    return True
